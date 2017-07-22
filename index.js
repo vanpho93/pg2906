51b3b91c2b2dbb636498f9c33e171db0c3545603
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-22 20:20:28 +0700

    forEach

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const queryDB = require('./db');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) => {
+    queryDB('SELECT * FROM "Product"', (err, result) => {
+        if (err) return res.send('LOI');
+        res.render('home');
+    });
+});
+
 app.listen(3000, () => console.log('Server started!'));
